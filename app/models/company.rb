990c03fb24347e1bf3147c2aef26a737773e6576
outa1127 <ota.ono@livesense.co.jp>
2026-07-15 16:26:28 +0900

    アソシエーションを追加

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,3 @@
 class Company < ApplicationRecord
+  has_many :job_postings, dependent: :destroy
 end
--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -1,3 +1,7 @@
 class JobPosting < ApplicationRecord
   belongs_to :company
+
+  scope :remote, -> { where(remote_ok: true) }
+  scope :salary_gte, ->(amount) { where(salary_min: amount..) }
+  scope :tech, ->(keyword) { where("tech_stack LIKE ?", "%#{sanitize_sql_like(keyword)}%") }
 end
